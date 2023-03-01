f81950edb7c82ee872126d18a86db82b14759703
YisusBlack18 <jesusangel18jz@gmail.com>
2023-03-01 14:48:16 -0700

    demo app

---
+++ Espressoft-master/app.py
@@ -0,0 +1,20 @@
+import sys
+from ventas import MainWindow
+from login import Ui_Form
+from PyQt5 import QtWidgets
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+
+    # comprueba que los datos de inicio de sesión sean correctos en la base de datos
+    # si es así, abre la ventana principal
+
+    if ui.pushButton.clicked:
+        ui = MainWindow()
+        ui.show()
+    
+    sys.exit(app.exec_())
