d20ef4635b8e34b38c09e2fe5b61484a6126705a
German <macsmacen@gmail.com>
2023-04-28 11:20:51 -0700

    metas sql
    prueba

--- expressoft.sql
+++ expressoft.sql
@@ -33,6 +33,21 @@ CREATE TABLE archivos_ventas (
   PRIMARY KEY (archivo_id)
 );
 
+CREATE TABLE metas (
+  id INT(11) AUTO_INCREMENT,
+  descripcion VARCHAR(50) NOT NULL,
+  meta_ventas DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO metas (descripcion, meta_ventas) VALUES
+('Metas diarias individuales', 1000.00),
+('Metas mensuales individuales', 30000.00),
+('Metas anuales individuales', 360000.00),
+('Metas diarias totales', 10000.00),
+('Metas mensuales totales', 300000.00),
+('Metas anuales totales', 3600000.00);
+
 INSERT INTO empleados (empleado_id, tipo_empleado, contrasenia, nombre, apellido_paterno, apellido_materno, estatus, telefono, email) VALUES
 ('1', 'Administrador', '123', 'Juan', 'Perez', 'Lopez', 1, 1234567890, 'hola@gmail.com');
 INSERT INTO empleados (empleado_id, tipo_empleado, contrasenia, nombre, apellido_paterno, apellido_materno, estatus, telefono, email) VALUES
